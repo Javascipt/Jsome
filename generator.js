7be9d905175b968e6884fc4c339bcf42bd851eda
Khalid REHIOUI <Rehioui@gmail.com>
2015-10-10 22:16:28 +0100

    Printing array or value on the current level

--- generator.js
+++ generator.js
@@ -107,7 +107,7 @@ module.exports = (function () {
           
         } else {
           
-          var coloredArray = colorifySpec('[', 'brack');
+          var coloredArray = colorifySpec('[', 'brack', level);
           for(var key in json) {
             coloredArray += [
                 colorify(json[key])
@@ -119,7 +119,7 @@ module.exports = (function () {
         }
         
       } else {
-        return colorify(json);
+        return generateLevel(level) + colorify(json);
       }
       
       return colored.join('\n');
