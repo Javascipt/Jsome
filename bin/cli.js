c590d1a9462b6658df2daf50d0409ae013b793cd
Nadeem Shabir <nadeem.shabir@talis.com>
2015-10-29 11:54:34 +0000

    adds yargs for more flexible cli support
    - using yargs to handle cli args
    - now support -l which you can use to show indentation levels
    - prints usage and help

--- bin/cli.js
+++ bin/cli.js
@@ -2,10 +2,31 @@
 
 var path      = require("path")
   , fs        = require("fs")
-  , jsome     = require("../script")
-  , filePath  = process.argv.slice(2).shift() || '';
+  , jsome     = require("../script");
 
-jsome.params.colored = true;
+var argv = require('yargs')
+  .usage('Usage: $0 [options] <file>')
+  .option('c',{
+    alias: 'colors',
+    default: true,
+    describe: 'color the output',
+    type: 'boolean'
+  })
+  .option('l',{
+    alias: 'levels',
+    default: false,
+    describe: 'show indentation levels',
+    type: 'boolean'
+  })
+  .example('$0 -cl /some/dir/file.json', 'print out the contents of file.json in color displaying indentation levels')
+  .example('$0 -c false -l /some/dir/file.json', 'print out the contents of file.json with no color but display indentation levels')
+  .help('h')
+  .argv;
+
+jsome.params.colored = argv.c;
+jsome.level.show     = argv.l;
+
+var filePath = argv._[0] || '';
 
 fs.exists(path.resolve(filePath), function (exists) {
   if(!exists) return jsome({error : "File doesn't exists"});
@@ -13,4 +34,4 @@ fs.exists(path.resolve(filePath), function (exists) {
     if(error) return jsome ({error : error.message});
     jsome.parse(jsonString.toString());
   });
-})
+});
