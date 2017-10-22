1c8ed9b4c9b2da5d2bd7a5ce88b6d0c08d0a883d
Drew J. Sonne <drew.sonne@gmail.com>
2017-10-22 14:41:21 +0100

    Simplified describe tree

--- test/test.js
+++ test/test.js
@@ -6,57 +6,54 @@ var assert = require('assert'),
   g = chalk.green,
   m = chalk.magenta
 
-describe('Jsome', function () {
+describe('Jsome run with', function () {
 
-  describe('run with', function () {
-    describe('spec non-compliant coloured output', function () {
-      var expected = y('{') +
-        '\n  ' + g('string') + y(': ') + y('"') + m('value') + y('"') +
-        y(',') +
-        '\n  ' + g('list') + y(': ') + y('[') + y('"') + m('one') + y('"') +
-        y(', ') + y('"') + m('two') +
-        y('"') + y(']') +
-        '\n' + y('}')
-      var actual = mJsome.getColoredString(
-        {'string': 'value', 'list': ['one', 'two']}
-      )
+  describe('spec non-compliant coloured output', function () {
+    var expected = y('{') +
+      '\n  ' + g('string') + y(': ') + y('"') + m('value') + y('"') +
+      y(',') +
+      '\n  ' + g('list') + y(': ') + y('[') + y('"') + m('one') + y('"') +
+      y(', ') + y('"') + m('two') +
+      y('"') + y(']') +
+      '\n' + y('}')
+    var actual = mJsome.getColoredString(
+      {'string': 'value', 'list': ['one', 'two']}
+    )
 
-      test('should return a simple non-standard json string', function () {
-        assert.equal(actual, expected)
-      })
-
-      test('should return a simple non-standard ANSI escaped, json string',
-        function () {
-          assert.equal(enc(actual), enc(expected))
-        })
-
+    test('should return a simple non-standard json string', function () {
+      assert.equal(actual, expected)
     })
 
-    describe('spec compliant coloured output', function () {
-      var expected = y('{') +
-        '\n  ' + y('"') + g('string') + y('"') + y(': ') + y('"') + m('value') +
-        y('"') +
-        y(',') +
-        '\n  ' + y('"') + g('list') + y('"') + y(': ') + y('[') + y('"') +
-        m('one') + y('"') +
-        y(', ') + y('"') + m('two') +
-        y('"') + y(']') +
-        '\n' + y('}')
-      mJsome.params.lintable = true
-      var actual = mJsome.getColoredString(
-        {'string': 'value', 'list': ['one', 'two']}
-      )
-
-      test('should return a simple non-standard json string', function () {
-        assert.equal(actual, expected)
+    test('should return a simple non-standard ANSI escaped, json string',
+      function () {
+        assert.equal(enc(actual), enc(expected))
       })
-
-      test('should return a simple non-standard ANSI escaped, json string',
-        function () {
-          assert.equal(enc(actual), enc(expected))
-        })
-    })
 
   })
 
+  describe('spec compliant coloured output', function () {
+    var expected = y('{') +
+      '\n  ' + y('"') + g('string') + y('"') + y(': ') + y('"') + m('value') +
+      y('"') +
+      y(',') +
+      '\n  ' + y('"') + g('list') + y('"') + y(': ') + y('[') + y('"') +
+      m('one') + y('"') +
+      y(', ') + y('"') + m('two') +
+      y('"') + y(']') +
+      '\n' + y('}')
+    mJsome.params.lintable = true
+    var actual = mJsome.getColoredString(
+      {'string': 'value', 'list': ['one', 'two']}
+    )
+
+    test('should return a simple non-standard json string', function () {
+      assert.equal(actual, expected)
+    })
+
+    test('should return a simple non-standard ANSI escaped, json string',
+      function () {
+        assert.equal(enc(actual), enc(expected))
+      })
+  })
+  
 })
